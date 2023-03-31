b716faf99e3e97ac42efaea864d401a9ae368d5f
Jason Gilchrist <jasonxgilchrist@gmail.com>
2023-03-31 06:47:21 +0530

    updated skills jsx and css

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -12,14 +12,27 @@ export default function Skills() {
           <li>C</li>
           <li>Python</li>
           <li>C# .NET</li>
+          <li>Go lang</li>
+          <li>R lang</li>
+          <li>C++</li>
+          <li>Java</li>
+        </div>
+        <div>
           <strong>Web Technologies</strong>
-          <li>HTML, CSS, JS</li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>Angular</li>
           <li>React Js</li>
           <li>Node JS</li>
+          <li>Express Js</li>
+          <li>Mongo DB</li>
         </div>
         <div>
           <strong>CRM Skills</strong>
-          <li>Microsoft Dynamics 365,Power Apps, Power Automate</li>
+          <li>Microsoft Dynamics 365</li>
+          <li>Power Apps</li>
+          <li>Power Automate</li>
           <li>Web Resources/Ribbon/Workflows/Cloud Flows/Plugins</li>
           <li>SQL Server,SSIS (using KingswaySoft)</li>
           <li>Cloud & Data Migration</li>
