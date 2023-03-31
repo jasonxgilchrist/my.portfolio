293a903f769357af188ac3940363cecedd485d9c
Jason Gilchrist <jasonxgilchrist@gmail.com>
2023-03-31 05:53:37 +0530

    changed skills 31-03-2023

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,42 +1,32 @@
-import React from 'react'
-import './style.css'
+import React from "react";
+import "./style.css";
 
 export default function Skills() {
   return (
-    <div className='skills'>
+    <div className="skills">
       <h1>Languages/Technologies/Skills</h1>
       <br />
-      <div className='content'>
+      <div className="content">
         <div>
           <strong>Programming Languages</strong>
           <li>C</li>
           <li>Python</li>
           <li>C# .NET</li>
-          <li>C++</li>
-          <li>Go lang</li>
-          <li>R lang</li>
-          <li>Java</li>
         </div>
         <div>
           <strong>Web Technologies</strong>
-          <li>HTML</li>
-          <li>HTML5</li>
-          <li>JavaScript</li>
-          <li>React JS</li>
-          <li>Angular</li>
+          <li>HTML, CSS, JS</li>
+          <li>React Js</li>
           <li>Node JS</li>
         </div>
         <div>
           <strong>CRM Skills</strong>
-          <li>Microsoft Dynamics 365</li>
-          <li>Power Apps</li>
-          <li>Power Automate</li>
+          <li>Microsoft Dynamics 365,Power Apps, Power Automate</li>
           <li>Web Resources/Ribbon/Workflows/Cloud Flows/Plugins</li>
-          <li>SSIS (using KingswaySoft)</li>
-          <li>SQL Server</li>
+          <li>SQL Server,SSIS (using KingswaySoft)</li>
           <li>Cloud & Data Migration</li>
         </div>
       </div>
     </div>
-  )
+  );
 }
